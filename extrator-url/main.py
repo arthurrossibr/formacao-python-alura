3f85fa4e57df55698abf37707b3475e10a455305
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2022-01-17 00:30:35 -0300

    Alguns métodos em strings

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,8 +1,19 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
-url_base = url[0:19]
-print(url_base)
-
-url_parametros = url[20:36]
+# Separa a base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = "moedaOrigem"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
