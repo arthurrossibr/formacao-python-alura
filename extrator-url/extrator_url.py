b335b33515c60c48c62554ca36012a32e8a6704a
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2022-01-17 12:32:17 -0300

    Finalizando manipulação de strings

--- extrator-url/extrator_url.py
+++ extrator-url/extrator_url.py
@@ -42,7 +42,35 @@ class ExtratorURL:
             valor = self.get_url_parametro()[indice_valor:indice_e_comercial]
         return valor
 
+    def converte_valor(self, origem, qtd, cambio):
+        if origem == "dolar":
+            return "R$ " + str(int(qtd) / int(cambio))
+        else:
+            return "$ " + str(int(qtd) * int(cambio))
 
-extrator_url = ExtratorURL("bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100")
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametro() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
+extrator_url = ExtratorURL(url)
+
+# print('O tamanho da URL é de {} caracteres'.format(len(extrator_url)))
+# print(extrator_url)
+# valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+# print(valor_quantidade)
+
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+convertido = extrator_url.converte_valor(moeda_origem, quantidade, VALOR_DOLAR)
+
+print(f"O valor de conversão de {moeda_origem} para {moeda_destino} "
+      f"com cambio de R${VALOR_DOLAR} por dolar, é de {convertido}")
